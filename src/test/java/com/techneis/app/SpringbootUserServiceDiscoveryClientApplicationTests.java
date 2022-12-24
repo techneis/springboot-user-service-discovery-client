653a6f14c7256c3193c2677f902e470919c3e378
Neeraj <techneis@gmail.com>
2022-12-25 00:08:47 +0530

    initial commit

---
+++ src/test/java/com/techneis/app/SpringbootUserServiceDiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techneis.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserServiceDiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
